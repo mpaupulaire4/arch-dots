2a3cc1094a560dff676c39c2134f116866c98e79
Michel Pupulaire <mpaupulaire4@gmail.com>
2022-04-03 08:40:16 -0500

    xorg setups

--- .config/nvim/lua/plugins/configs/nvim-cmp.lua
+++ .config/nvim/lua/plugins/configs/nvim-cmp.lua
@@ -35,8 +35,8 @@ cmp.setup {
     end
   },
   mapping = {
-    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    ['<PageUp>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    ['<PageDown>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
     ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
     -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
     ['<C-y>'] = cmp.config.disable,
