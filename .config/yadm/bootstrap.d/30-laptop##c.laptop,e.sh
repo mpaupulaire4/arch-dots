e494b55fbed80cc57216ed7b06eafabd63cf22fe
Michel Pupulaire <mpaupulaire4@gmail.com>
2023-03-19 23:14:35 -0400

    ignore and laptop notes

--- .config/yadm/bootstrap.d/30-laptop##c.laptop,e.sh
+++ .config/yadm/bootstrap.d/30-laptop##c.laptop,e.sh
@@ -10,5 +10,8 @@
 if ! systemctl is-enabled keyd.service >/dev/null 2>&1; then
   paru -S keyd --needed
   sudo systemctl enable keyd.service
+  # TODO: nove to a separate if condition.
+  # if the files aren't the same then
+  # override what is there with what is in the config dir
   sudo cp $HOME/.config/keyd/default.conf /etc/keyd/default.conf
 fi
