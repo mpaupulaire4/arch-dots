98b72916aff7da2e2ccc6459e6f087b186b73a70
Michel Pupulaire <mpaupulaire4@gmail.com>
2023-05-29 00:51:01 -0400

    devenv stuff

--- .config/yadm/bootstrap.d/30-laptop##c.laptop,e.sh
+++ .config/yadm/bootstrap.d/30-laptop##c.laptop,e.sh
@@ -10,10 +10,7 @@
 if ! systemctl is-enabled keyd.service >/dev/null 2>&1; then
   paru -S keyd --needed
   sudo systemctl enable keyd.service
-  # TODO: nove to a separate if condition.
-  # if the files aren't the same then
-  # override what is there with what is in the config dir
-  sudo cp $HOME/.config/keyd/default.conf /etc/keyd/default.conf
+  sudo ln -sf "$HOME/.config/keyd/default.conf" /etc/keyd/default.conf
 fi
 
 if ! systemctl is-enabled ufw.service >/dev/null 2>&1; then
